04eb9614a0b7c2cbaea54b8f94b3dc3999503aff
jennifertakagi <jennitakagi@gmail.com>
2020-08-07 18:47:44 -0300

    Approach logger on whole routes

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 import express from 'express';
-import gradesRouter from './routes/grades.js'
+import winston from 'winston';
+import gradesRouter from './routes/grades.js';
 import { promises as fs } from 'fs';
 
 const app = express();
+const { combine, timestamp, label, printf } = winston.format;
+const formatter = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
 global.fileName = './data/grades.json';
+global.logger = winston.createLogger({
+    level: 'silly',
+    format: combine(
+        label({ label: 'grade-control.log'}),
+        timestamp(),
+        formatter
+    ),
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'grades-control.log' }),
+    ]
+});
 
 app.use(express.json());
 app.use('/grade', gradesRouter);
 
 app.listen(3031, async () => {
     try {
-        await fs.readFile(global.fileName);
-        console.log('API started');
+        await fs.readFile(fileName);
+
+        logger.info('API started');
     } catch(error) {
         const initialJSON = {
             nextId: 1,
             grades: []
         };
-        fs.writeFile(global.fileName, JSON.stringify(initialJSON))
+        fs.writeFile(fileName, JSON.stringify(initialJSON))
             .then(() => {
-                console.log('API started and file created!');
+                logger.info('API started and file created!');
             })
             .catch((error) => {
-                console.log(error);
+                logger.error(error);
             });
     }
 });
--- routes/grades.js
+++ routes/grades.js
@@ -6,14 +6,16 @@ const router = express.Router();
 router.post("/create", async (req, res, next) => {
     try {
         let newGrade = req.body;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
         
         newGrade = { id: dataJSON.nextId++, ...newGrade, timestamp: new Date() };
         dataJSON.grades.push(newGrade);
 
-        await fs.writeFile(global.fileName, JSON.stringify(dataJSON, null, 2));
+        await fs.writeFile(fileName, JSON.stringify(dataJSON, null, 2));
 
         res.send(newGrade);
+
+        logger.info(`POST /grade/create - ${JSON.stringify(newGrade)}`);
     } catch (error) {
         next(error);
     }
@@ -22,12 +24,14 @@ router.post("/create", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
     try {
         const id = req.params.id;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
         const selectedGrades = dataJSON.grades.find(grade => grade.id === Number(id));
 
         delete dataJSON.nextId;
 
         res.send(selectedGrades);
+
+        logger.info(`GET /grade/:id - ${id}`);
     } catch (error) {
         next(error);
     }
@@ -36,7 +40,7 @@ router.get("/:id", async (req, res, next) => {
 router.get("/totalGrade/:student/:subject", async (req, res, next) => {
     try {
         const { student, subject } = req.params;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
         const totalGrade = dataJSON.grades.reduce((sumGrade, grade) => {
             if (grade.student === student && grade.subject === subject) {
                 sumGrade += grade.value;
@@ -45,6 +49,8 @@ router.get("/totalGrade/:student/:subject", async (req, res, next) => {
         }, 0);
 
         res.send(`The sum of ${student} in ${subject} is: ${totalGrade}`);
+
+        logger.info(`GET /grade/totalGrade/:student/:subject - ${totalGrade}`);
     } catch (error) {
         next(error);
     }
@@ -53,12 +59,14 @@ router.get("/totalGrade/:student/:subject", async (req, res, next) => {
 router.get("/meanGrade/:subject/:type", async (req, res, next) => {
     try {
         const { subject, type } = req.params;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
         const filterGrades = dataJSON.grades.filter(grade => grade.subject === subject && grade.type === type);
         const totalGrade = filterGrades.reduce((a, b) => a + b.value, 0);
-        const meanGrade = (totalGrade / filter.length) || null
+        const meanGrade = (totalGrade / filterGrades.length) || null
 
         res.send(`The mean value of ${subject} in ${type} is: ${meanGrade}`);
+
+        logger.info(`GET /grade/meanGrade/:subject/:type - ${meanGrade}`);
     } catch (error) {
         next(error);
     }
@@ -67,12 +75,14 @@ router.get("/meanGrade/:subject/:type", async (req, res, next) => {
 router.get("/betterGrades/:subject/:type", async (req, res, next) => {
     try {
         const { subject, type } = req.params;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
         const filterGrades = dataJSON.grades.filter(grade => grade.subject === subject && grade.type === type);
         const sortedGrade = filterGrades.sort((a, b) => a.value > b.value ? -1 : 1);
         const betterThree = sortedGrade.slice(0, 3);
 
         res.send(betterThree);
+
+        logger.info(`GET /grade/betterGrades/:subject/:type - ${betterGrades}`);
     } catch (error) {
         next(error);
     }
@@ -81,12 +91,20 @@ router.get("/betterGrades/:subject/:type", async (req, res, next) => {
 router.delete("/delete/:id", async (req, res, next) => {
     try {
         const id = req.params.id;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
-        dataJSON.grades = dataJSON.grades.filter(grade => grade.id !== Number(id));
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
+        const userIndex = validateUserExists(dataJSON.grades, id)
 
-        await fs.writeFile(global.fileName, JSON.stringify(dataJSON, null, 2));
+        if (userIndex > -1) {
+            dataJSON.grades = dataJSON.grades.filter(grade => grade.id !== Number(id));
 
-        res.end();
+            await fs.writeFile(fileName, JSON.stringify(dataJSON, null, 2));
+    
+            res.send('User deleted successfully!');
+
+            logger.info(`DELETE /grade/delete/:id - ${id}`);
+        } else {
+            throw new Error('This id does not exist!');
+        }
     } catch (error) {
         next(error);
     }
@@ -96,18 +114,21 @@ router.patch("/updateGrade/:id", async (req, res, next) => {
     try {
         const newGrade = req.body;
         const id = req.params.id;
-        const dataJSON = JSON.parse(await fs.readFile(global.fileName));
-        const indexGrade = dataJSON.grades.findIndex(grade => grade.id === Number(id))
+        const dataJSON = JSON.parse(await fs.readFile(fileName));
+        const userIndex = validateUserExists(dataJSON.grades, id)
+        const updateGrade = dataJSON.grades[userIndex]
 
-        if (indexGrade > -1) {
-            dataJSON.grades[indexGrade].student = newGrade.student;
-            dataJSON.grades[indexGrade].subject = newGrade.subject;
-            dataJSON.grades[indexGrade].type = newGrade.type;
-            dataJSON.grades[indexGrade].value = newGrade.value;
+        if (updateGrade) {
+            updateGrade.student = newGrade.student;
+            updateGrade.subject = newGrade.subject;
+            updateGrade.type = newGrade.type;
+            updateGrade.value = newGrade.value;
 
-            await fs.writeFile(global.fileName, JSON.stringify(dataJSON, null, 2));
+            await fs.writeFile(fileName, JSON.stringify(dataJSON, null, 2));
         
-            res.send(dataJSON.grades[indexGrade]);
+            res.send(updateGrade);
+
+            logger.info(`UPDATE /grade/updateGrade/:id - ${JSON.stringify(dataJSON.grades[indexGrade])}`);
         } else {
             throw new Error('This id does not exist!');
         }
@@ -117,8 +138,18 @@ router.patch("/updateGrade/:id", async (req, res, next) => {
 })
 
 router.use((error, req, res, next) => {
-    console.log(error);
+    logger.error(`${req.method} ${req.baseUrl} ${error.message}`);
     res.status(400).send({ error: error.message});
 });
+
+/**
+ * Validate if user exists on JSON file
+ * @param {Array} grades list of all grades
+ * @param {String} userId user's id
+ * @returns {Number} index of user, if it does not exits returns -1
+ */
+function validateUserExists(grades = [], userId = '') {
+    return grades.findIndex(grade => grade.id === Number(userId))
+}
 
 export default router;
